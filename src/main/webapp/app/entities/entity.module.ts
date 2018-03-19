07996cbee7f4420afc9b11d83c913def8ccdfc22
shakeys <shakeys@Shakeys>
2018-03-19 15:18:38 +0800

    Add product entity

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -3,6 +3,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RemitBlogModule } from './blog/blog.module';
 import { RemitEntryModule } from './entry/entry.module';
 import { RemitTagModule } from './tag/tag.module';
+import { RemitProductModule } from './product/product.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
@@ -10,6 +11,7 @@ import { RemitTagModule } from './tag/tag.module';
         RemitBlogModule,
         RemitEntryModule,
         RemitTagModule,
+        RemitProductModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
